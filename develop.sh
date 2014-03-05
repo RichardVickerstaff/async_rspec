fc3e6a816bc8b6479c6ef98aad7eab45446d03c9
RichardVickerstaff <RichardVickerstaff@users.noreply.github.com>
2014-03-05 14:55:39 +0000

    remove tmux

--- develop.sh
+++ develop.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
 
-project_root=$1
-tmux_session=$(basename $project_root)
-
-pushd $project_root >/dev/null
-
 if [ -p commands ]; then
   rm commands
 fi
 
 mkfifo commands
 
-export TERM=xterm-256color
-
-tmux has-session -t $tmux_session
-if [ $? != 0 ]; then
-  window=1
-  tmux new-session -s $tmux_session -n tests -d
-  tmux send-keys -t ${tmux_session}:$window "while true; do sh -c '$(cat commands)'; done" C-m
-
-  window=2
-  tmux new-window -t ${tmux_session} -n code
-  tmux send-keys -t ${tmux_session}:$window 'vim' C-m
-
-  window=3
-  tmux new-window -t $tmux_session -n shell
-
-  tmux select-window -t ${tmux_session}:2
-fi
-tmux attach -t $tmux_session
-
-popd >/dev/null
+while true; do sh -c '$(cat commands)'; done
